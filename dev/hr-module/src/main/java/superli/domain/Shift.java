4bfcf92279f381a35f942611898c1c52649aff74
zeenyaseen <zeenashraf2003@gmail.com>
2026-04-12 19:56:25 +0300

    add Shift domain basic implementation

--- dev/hr-module/src/main/java/superli/domain/Shift.java
+++ dev/hr-module/src/main/java/superli/domain/Shift.java
@@ -1,5 +1,42 @@
 package superli.domain;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Shift {
-    
+    private LocalDate date ;
+    private ShiftType shiftType ;
+    private List<ShiftAssignment> assignments ;
+    private Map<Role,Integer> requiredRoles ;
+
+    public Shift(LocalDate date , ShiftType shiftType){
+        this.date = date ;
+        this.shiftType = shiftType ;
+        this.assignments = new ArrayList<>();
+        this.requiredRoles = new HashMap<>();
+    }
+
+    public LocalDate getDate(){
+        return date ;
+    }
+    public ShiftType getShiftType(){
+        return shiftType ;
+    }
+    public List<ShiftAssignment> getAssignments(){
+        return assignments ;
+    }
+    public Map<Role,Integer> getRequiredRoles(){
+        return requiredRoles ;
+    }
+
+    public void addAssignment(ShiftAssignment assignment){
+        assignments.add(assignment);
+    }
+    public void addRequiredRole(Role role , int amount){
+        requiredRoles.put(role, amount);
+    }
+
 }
--- dev/hr-module/src/main/java/superli/domain/ShiftAssignment.java
+++ dev/hr-module/src/main/java/superli/domain/ShiftAssignment.java
@@ -1,5 +1,19 @@
 package superli.domain;
 
 public class ShiftAssignment {
+    private Employee employee ;
+    private Role role ;
+
+    public ShiftAssignment(Employee e , Role r){
+        this.employee = e ;
+        this.role = r ;
+    }
     
+    public Employee getEmployee(){
+        return employee;
+    }
+    public Role getRole(){
+        return role;
+    }
+
 }
