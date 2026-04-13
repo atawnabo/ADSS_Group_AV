08baba82bdeb9c025718e4205cb624c4319fbca1
zeenyaseen <zeenashraf2003@gmail.com>
2026-04-13 16:49:43 +0300

    Add Shift controller and service basic implementation

--- dev/hr-module/src/main/java/superli/controller/ShiftController.java
+++ dev/hr-module/src/main/java/superli/controller/ShiftController.java
@@ -1,5 +1,51 @@
 package superli.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import superli.domain.*;
+
 public class ShiftController {
-    
+    private List<Shift> shifts ;
+
+    public ShiftController(){
+        this.shifts = new ArrayList<>();
+    }
+
+    public void createShift(LocalDate date , ShiftType shiftType){
+        if(getShift(date,shiftType) != null){
+            throw new IllegalArgumentException("Shift already exists");
+        }
+        Shift newsShift = new Shift(date, shiftType);
+        shifts.add(newsShift);
+    }
+
+    public Shift getShift(LocalDate date , ShiftType shiftType){
+        for(Shift shift : shifts){
+            if(shift.getDate().isEqual(date) && shift.getShiftType()==shiftType){
+                return shift ;
+            }
+        }
+        return null ;
+    }
+    public void addRequiredRole(LocalDate date ,ShiftType shiftType ,Role role ,int amount){
+        Shift shift = getShift(date, shiftType) ;
+        if(shift == null){
+            throw new IllegalArgumentException("Shift does not exists");
+        }
+        shift.addRequiredRole(role,amount);
+    }
+
+    public void addAssignment(LocalDate date ,ShiftType shiftType ,ShiftAssignment assignment){
+        Shift shift = getShift(date, shiftType) ;
+        if(shift == null){
+            throw new IllegalArgumentException("Shift does not exists");
+        }
+        shift.addAssignment(assignment);
+    }
+
+    public List<Shift> getShifts(){
+        return new ArrayList<>(shifts) ;
+    }
+
 }
--- dev/hr-module/src/main/java/superli/service/ShiftService.java
+++ dev/hr-module/src/main/java/superli/service/ShiftService.java
@@ -1,5 +1,36 @@
 package superli.service;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import superli.controller.ShiftController;
+import superli.domain.*;
+
 public class ShiftService {
     
+    private ShiftController shiftController;
+
+    public ShiftService(){
+        this.shiftController = new ShiftController();
+    }
+
+    public void createShift(LocalDate date , ShiftType shiftType){
+        shiftController.createShift(date, shiftType);
+    }
+
+    public Shift getShift(LocalDate date , ShiftType shiftType){
+       return shiftController.getShift(date, shiftType);
+    }
+
+    public void addRequiredRole(LocalDate date ,ShiftType shiftType ,Role role ,int amount){
+        shiftController.addRequiredRole(date, shiftType, role, amount);
+    }
+
+    public void addAssignment(LocalDate date ,ShiftType shiftType ,ShiftAssignment assignment){
+        shiftController.addAssignment(date, shiftType, assignment);
+    }
+
+    public List<Shift> getShifts(){
+        return shiftController.getShifts();
+    }
 }
